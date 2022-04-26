0897a192fa5ab5d110f37233e9cf0e955cecfd8e
test <test@test.com>
2022-04-26 23:54:35 +0800

    modify the ch_1

--- src/simple/ch_1_variable.rs
+++ src/simple/ch_1_variable.rs
@@ -1,14 +1,12 @@
 #[cfg(test)]
-mod variable {
-    #[test]
-    fn var() {
-        let mut a = 1;
-        let b = false;
-        // bool can not use as to convert to u8;
-        // assert_eq!(a as bool,b);
-        // assert_eq!(a, b as bool);
-        // assert!(b as u8);
-        // assert!(a as bool);
-        assert_eq!(a<0,b);
-    }
+#[test]
+fn var() {
+    let mut a = 1;
+    let b = false;
+    // bool can not use as to convert to u8;
+    // assert_eq!(a as bool,b);
+    // assert_eq!(a, b as bool);
+    // assert!(b as u8);
+    // assert!(a as bool);
+    assert_eq!(a < 0, b);
 }
