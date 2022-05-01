56fc2eedf70dce34e4028125db3a5445bf0aca34
test <test@test.com>
2022-05-01 14:49:45 +0800

    learn more about vector

--- src/simple/ch_9_vector.rs
+++ src/simple/ch_9_vector.rs
@@ -1,9 +1,13 @@
 #[test]
 fn hello() {
     let word = "hello world".to_string();
-    let num = (0..10).collect::<Vec<i32>>();
+    let mut num = (5..10).collect::<Vec<i32>>();
     println!("Collected numbers are {:?}", num);
-    for x in num.iter() {
+    for (x, index) in num.iter().enumerate() {
+        println!("Collected numbers are {:?} {:?}", x, index);
+    }
+    for x in num.iter_mut() {
+        *x = *x * 3;
         println!("Collected numbers are {:?}", x);
     }
 }
