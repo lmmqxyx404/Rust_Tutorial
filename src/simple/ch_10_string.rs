1ff05918749216bc2f7ea239cacd090407033497
lmmqxyx <sjsqywh@outlook.com>
2022-05-04 00:03:45 +0800

    learn more about hashmap

--- src/simple/ch_10_string.rs
+++ src/simple/ch_10_string.rs
@@ -2,12 +2,25 @@
  * @Author: Lmmqxyx
  * @Date: 2022-05-02 23:54:25
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-05-02 23:55:18
+ * @LastEditTime: 2022-05-03 23:59:38
  * @FilePath: \Rust_Tutorial\src\simple\ch_10_string.rs
  * @Description: 
  */
 #[test]
 fn hello() {
-    let p: &'static str="hello world";
+    let p: &'static str="Hello world madam!";
     println!("{}",p);
+    for word in p.split_whitespace().rev() {
+        println!("> {}",word);
+    }
+    let mut chars=p.chars().collect::<Vec<char>>();
+    chars.sort();
+    chars.dedup();
+    let mut string =String::new();
+    for c in chars {
+        string.push(c);
+        string.push_str(", ");
+        println!("{}",c);
+    }
+    println!("{}",string);
 }
---
+++ src/simple/ch_11_HashMap.rs
@@ -0,0 +1,14 @@
+/*
+ * @Author: Lmmqxyx
+ * @Date: 2022-05-04 00:00:32
+ * @LastEditors: 
+ * @LastEditTime: 2022-05-04 00:02:51
+ * @FilePath: \Rust_Tutorial\src\simple\ch_11_hashmap.rs
+ * @Description: 
+ */
+use std::collections::HashMap;
+#[test]
+fn main() {
+    let mut contacts=HashMap::new();
+    contacts.insert("Bob","1336574");
+}
