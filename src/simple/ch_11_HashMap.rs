7cb574a3732ee0a6a27e3822459cbb831350dcd0
lmmqxyx <2793037386@qq.com>
2022-05-05 14:27:32 +0800

    learn more about hashmap

--- src/simple/ch_11_HashMap.rs
+++ src/simple/ch_11_HashMap.rs
@@ -1,14 +1,27 @@
 /*
  * @Author: Lmmqxyx
  * @Date: 2022-05-04 00:00:32
- * @LastEditors: 
- * @LastEditTime: 2022-05-04 00:02:51
- * @FilePath: \Rust_Tutorial\src\simple\ch_11_hashmap.rs
- * @Description: 
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-05-05 14:07:26
+ * @FilePath: \Rust_Tutorial\src\simple\ch_11_HashMap.rs
+ * @Description:
  */
 use std::collections::HashMap;
 #[test]
 fn main() {
-    let mut contacts=HashMap::new();
-    contacts.insert("Bob","1336574");
+    let mut contacts = HashMap::new();
+    contacts.insert("Bob", "1336574");
+    contacts.insert("Alice", "12345678");
+    match contacts.get(&"Bob") {
+        Some(number) => {
+            println!("Bob's number is {:?}", number);
+        }
+        _ => {
+            println!("There is no Bob's number!");
+        }
+    }
+    contacts.remove(&"Bob");
+    for (&a, b) in contacts.iter() {
+        println!("{}'s number is {}", &a, &b);
+    }
 }
--- src/simple/ch_6_constants.rs
+++ src/simple/ch_6_constants.rs
@@ -2,11 +2,11 @@
  * @Author: Lmmqxyx
  * @Date: 2022-04-13 17:57:15
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-04-23 23:29:13
+ * @LastEditTime: 2022-05-05 14:02:53
  * @FilePath: \Rust_Tutorial\src\simple\ch_6_constants.rs
  * @Description:
  */
-static LANGUAGE: &str = "rustc-std-workspace-alloc";
+static LANGUAGE: &str = "RUST";
 const YEAR: u32 = 2022;
 #[test]
 fn main() {
