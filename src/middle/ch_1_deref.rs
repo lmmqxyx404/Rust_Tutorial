c37bd60ac4869492a40d76eddebd4cd7ea757039
lmmqxyx <1368470804@qq.com>
2022-06-27 23:53:02 +0800

    optimized the project

--- src/middle/ch_1_deref.rs
+++ src/middle/ch_1_deref.rs
@@ -2,11 +2,11 @@
  * @Author: Lmmqxyx
  * @Date: 2022-06-21 07:42:10
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-06-21 07:44:10
+ * @LastEditTime: 2022-06-27 23:52:41
  * @FilePath: \Rust_Tutorial\src\middle\ch_1_deref.rs
  * @Description: 
  */
 #[test]
-fn middle_1(){
+fn middle_1_deref(){
     println!("This is middle");
 }
