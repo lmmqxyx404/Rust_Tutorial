f0060a504ad3c58b506fe71c5edaf4718aebd189
lmmqxyx <1368470804@qq.com>
2022-06-27 23:59:17 +0800

    optimized the project

--- src/middle/ch_1_deref.rs
+++ src/middle/ch_1_deref.rs
@@ -2,11 +2,14 @@
  * @Author: Lmmqxyx
  * @Date: 2022-06-21 07:42:10
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-06-27 23:52:41
+ * @LastEditTime: 2022-06-27 23:58:18
  * @FilePath: \Rust_Tutorial\src\middle\ch_1_deref.rs
  * @Description: 
  */
 #[test]
 fn middle_1_deref(){
-    println!("This is middle");
+    let mut c="ccccc";
+    let mut calias=&c;
+    calias=&"ddd";
+    println!("This is middle {}",calias);
 }
--- src/middle/mod.rs
+++ src/middle/mod.rs
@@ -1,1 +1,10 @@
+/*
+ * @Author: Lmmqxyx
+ * @Date: 2022-06-21 07:42:38
+ * @LastEditors: 
+ * @LastEditTime: 2022-06-27 23:57:32
+ * @FilePath: \Rust_Tutorial\src\middle\mod.rs
+ * @Description: 
+ */
 mod ch_0_hello;
+mod ch_1_deref;
