7f81d66102d361f9e3712fc5967d6aaa19694765
lmmqxyx <1368470804@qq.com>
2022-08-12 17:10:11 +0800

    learn more about derefrence and ownership

---
+++ src/hard/ch_4_copy_clone.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+struct Node {
+    val: i32,
+}
+
+impl Node {
+    fn new(val: i32) -> Self {
+        Node { val }
+    }
+}
+#[test]
+fn main() {
+    // println!("{:}", Node::new(5)); must add Debug trait
+    println!("{:?}", Node::new(5));
+
+    let a = Node::new(5);
+    let b = a;
+    // a has been moved to b.
+    // assert_eq!(b.val,a.val);
+    let dd = &b;
+    // automatic dereference
+    assert_eq!(b.val, (*dd).val);
+    assert_eq!(b.val, dd.val);
+
+    let aa = 5;
+    let ss = aa;
+    // because i32 has Copy Trait
+    assert_eq!(aa, ss);
+    let ss = &aa;
+    // assert_eq!(aa, ss);
+}
