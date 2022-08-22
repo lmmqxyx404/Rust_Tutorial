aaba980358cbeb351c11a91eccaeeebf1772aa2b
lmmqxyx <1368470804@qq.com>
2022-08-22 23:57:57 +0800

    learn more vec

--- src/hard/ch_2_vec_iter.rs
+++ src/hard/ch_2_vec_iter.rs
@@ -10,5 +10,11 @@
 fn ch_2_vec_iter() {
     let a = vec![String::from("hello"), String::from("world")];
     let sum = &a.iter().fold(String::from(""), |acc, x| acc + " " + x);
+
+    let av: Vec<u8> = vec![1, 2, 4, 8];
+    let p = av.as_slice();
+
+    println!("{:?}", p);
+
     assert_eq!(sum, " hello world")
 }
