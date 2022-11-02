acea8d52b236d7b498283e15820fe94ba21becaf
lmmqxyx <1368470804@qq.com>
2022-11-02 23:17:02 +0800

    add a unsafe demo

--- src/hard/ch_7_unsafe.rs
+++ src/hard/ch_7_unsafe.rs
@@ -1,4 +1,12 @@
+static mut COUNTER: u32 = 0;
+
+fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
+}
 #[test]
 fn hello() {
+    add_to_count(12);
     println!("Hard trip starts.")
 }
