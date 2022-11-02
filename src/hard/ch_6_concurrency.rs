ad6f01dee504edfd262106cac1a1c83e113df26a
lmmqxyx <1368470804@qq.com>
2022-11-02 23:08:51 +0800

    learn more about concurrency.

--- src/hard/ch_6_concurrency.rs
+++ src/hard/ch_6_concurrency.rs
@@ -1,8 +1,8 @@
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -71,12 +71,33 @@ mod tests {
 
     #[test]
     fn concunrency_5() {
-        use std::sync::Mutex;
         let m = Mutex::new(5);
         {
             let mut num = m.lock().unwrap();
             *num = 100;
         }
         println!("mu ={:?}", m);
+    }
+
+    #[test]
+    fn concunrency_6() {
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+        println!("Result is: {}", *counter.lock().unwrap());
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result is: {}", *counter.lock().unwrap());
     }
 }
