d7db5cabf9f70ea6f0f8f1a76d92ec8146864d80
lmmqxyx <1368470804@qq.com>
2022-11-05 20:52:54 +0800

    Learn more about commands.

--- src/hard/ch_9_std_misc.rs
+++ src/hard/ch_9_std_misc.rs
@@ -27,3 +27,22 @@ fn path() {
         Ok(file) => file,
     };
 }
+
+use std::process::Command;
+#[test]
+fn process() {
+    let output = Command::new("g++")
+        .arg("--version")
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+
+        print!("rustc succeeded and stdout was:\n{}", s);
+    } else {
+        let s = String::from_utf8_lossy(&output.stderr);
+
+        print!("rustc failed and stderr was:\n{}", s);
+    }
+}
