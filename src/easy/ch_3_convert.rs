493e31306ea00fe9a05e7906f5e8d3cb71c9c0df
lmmqxyx <1368470804@qq.com>
2023-03-23 15:28:58 +0800

    learn more about From trait

--- src/easy/ch_3_convert.rs
+++ src/easy/ch_3_convert.rs
@@ -6,6 +6,8 @@
  * @FilePath: \Rust_Tutorial\src\simple\ch_3_convert.rs
  * @Description:
  */
+
+// basic type conversions
 fn char_to_u8(a: char) -> u8 {
     a as u8
 }
@@ -17,6 +19,19 @@ fn i32_to_char(a: i32) -> char {
 fn string_to_char_array(_s: String) -> Vec<char> {
     vec!['a']
 }
+
+struct pp {}
+
+struct qq {
+    qq: i32,
+}
+
+impl From<pp> for qq {
+    fn from(value: pp) -> Self {
+        Self { qq: 32 }
+    }
+}
+
 #[test]
 fn var() {
     let mut a = 'a';
@@ -24,4 +39,9 @@ fn var() {
     assert_eq!(97, char_to_u8(a));
     assert_eq!(a, i32_to_char(97));
     assert_eq!(1, std::mem::size_of_val(&(a as u8)));
+
+    
+    let p: qq = pp {}.into();
+    let q = qq { qq: 32 };
+    assert_eq!(q.qq, p.qq);
 }
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,6 +1,9 @@
 mod ch_0_hello;
 mod ch_1_variable;
 mod ch_2_loop;
+
+/// The convertion of basic type could be simple(like i64,i32).
+/// use as keyword. i64 as u8.
 mod ch_3_convert;
 mod ch_4_struct;
 mod ch_5_enum;
